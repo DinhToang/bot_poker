69534bd3da6d36c7db0bba46acb6b342862170d7
AsheNguyen <toan.nguyendinh@ncc.asia>
2026-01-09 14:08:38 +0700

    change env

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,12 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
   imports: [
     ConfigModule.forRoot({
       validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
+        // POSTGRES_HOST: Joi.string().required(),
+        // POSTGRES_PORT: Joi.number().required(),
+        // POSTGRES_USER: Joi.string().required(),
+        // POSTGRES_PASSWORD: Joi.string().required(),
+        // POSTGRES_DB: Joi.string().required(),
+        PRISMA_DATABASE_URL: Joi.string().required(),
         MEZON_TOKEN: Joi.string().required(),
       }),
     }),
@@ -24,13 +25,27 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
+         url:
+          configService.get<string>('PRISMA_DATABASE_URL') ||
+          configService.get<string>('POSTGRES_URL'),
+
         autoLoadEntities: true,
-        synchronize: true,
+
+        // ❌ KHÔNG dùng production
+        synchronize: false,
+
+        // ✅ BẮT BUỘC cho Vercel Postgres
+        ssl:
+          process.env.NODE_ENV === 'production'
+            ? { rejectUnauthorized: false }
+            : false,
+        // host: configService.get('POSTGRES_HOST'),
+        // port: configService.get('POSTGRES_PORT'),
+        // username: configService.get('POSTGRES_USER'),
+        // password: configService.get('POSTGRES_PASSWORD'),
+        // database: configService.get('POSTGRES_DB'),
+        // autoLoadEntities: true,
+        // synchronize: true,
         // migrations: [path.join(__dirname, 'src', 'migration', '*.js')],
         // cli: {
         //   migrationsDir: __dirname + '/migration',
